ebdcb4d5528feb34e685f9c7b1ec5b8673a2268a
Attila <59293104+sweeptic@users.noreply.github.com>
2021-02-11 09:46:37 +0100

    Context page example: pass props several levels down

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const Page = () => {
+  const user = 'Max';
+  const avatarSize = '33';
+  const content = <Feed user={user} />;
+
+  const topBar = (
+    <NavigationBar>
+      <Link>
+        <Avatar user={user} size={avatarSize} />
+      </Link>
+    </NavigationBar>
+  );
+
+  return <PageLayout topBar={topBar} content={content} />;
+};
+
+const PageLayout = props => {
+  const { topBar, content } = props;
+  return (
+    <div>
+      {topBar} <br /> {content}
+    </div>
+  );
+};
+
+const NavigationBar = props => {
+  return <div>{props.children}</div>;
+};
+
+const Feed = props => {
+  return (
+    <div>
+      This is {props.user} feeds:
+      <ul>
+        <li>feed line 1 </li>
+        <li>feed line 2 </li>
+        <li>feed line 3 </li>
+      </ul>
+    </div>
+  );
+};
+
+const Link = props => {
+  return <div>{props.children}</div>;
+};
+
+const Avatar = props => {
+  const { user, size } = props;
+  return (
+    <div>
+      the user is: {user}, avatar size is : {size}
+    </div>
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <Page />
+  </div>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
